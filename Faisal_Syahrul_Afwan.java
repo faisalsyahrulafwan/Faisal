4d56f08df05e6e262037357a63645771b1f921e9
Faisal_Syahrul_Afwan <48313590+faisalsyahrulafwan@users.noreply.github.com>
2019-03-07 17:45:17 -0800

    Add files via upload

---
+++ Faisal_Syahrul_Afwan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faisal_syahrul_afwan;
+
+/**
+ *
+ * @author GL62M-7REX
+ */
+public class Faisal_Syahrul_Afwan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama    :FAISAL SYAHRUL AFWAN");
+        System.out.println("Nim     :17090062");
+        System.out.println("Kelas   :4A");
+        // TODO code application logic here
+    }
+    
+}
